7dd161ce12267b3966e04baa7abae238d29c5580
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-05-28 15:28:17 -0600

    Add unit 1 evaluation functions and tests

---
+++ demos/src/eval.test.js
@@ -0,0 +1,28 @@
+const {subtract, isEven, max} = require('./eval.js');
+
+// pruebas actividad 1
+test('subtract 10-3 to equal 7', () => {
+  expect(subtract(10, 3)).toBe(7);
+});
+
+test('subtract 3-10 to equal -7', () => {
+  expect(subtract(3, 10)).toBe(-7);
+});
+
+// pruebas actividad 2
+test('check if 10 is even', () => {
+  expect(isEven(10)).toBe(true);
+});
+
+test('check if 3 is even', () => {
+  expect(isEven(3)).toBe(false);
+});
+
+// pruebas actividad 3
+test('find max value of array [4, 8, 1] should return 8', () => {
+  expect(max([4, 8, 1])).toBe(8);
+});
+
+test('find max value of array [] should return null', () => {
+  expect(max([])).toBe(null);
+});
