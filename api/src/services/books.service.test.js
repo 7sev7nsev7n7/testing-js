977d7e9beca518edfdc6b261bf5a7630012bb885
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-06-16 21:08:33 -0600

    Mocking

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,5 +1,23 @@
 const BooksService = require('./books.service');
 
+let fakeBooks = [
+  {
+    _id: 1,
+    title: 'Harry Potter',
+    author: 'J. K. Rowling'
+  }
+]
+
+let newBook = {_id: 2, title: '1984', author: 'George Orwell'};
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks], 
+  // create: () => fakeBooks.push({_id: 2, title: '1984', author: 'George Orwell'})
+  create: ()=>fakeBooks.push(newBook)
+}
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
@@ -7,10 +25,19 @@ describe('Test for BooksService', () => {
   });
 
   describe('Test for getBooks()', () => {
-    test('Should return a list of books', async() => {
-      const books = await service.getBooks({});
-      console.log(books);
-      expect(books.length).toEqual(1);
+    test('Should return a list of books', () => {
+      const book = service.createBook({});
+      console.log(book);
+      // expect(book.title).toEqual('1984'); // This is considering the new book created
     });
   });
+
+  describe('Test for getBooks()', () => {
+    test('Should return all books', () => {
+      let books = service.getBooks({});
+      console.log(books);
+      expect(books.length).toEqual(2); // This is considering the new book created
+    });
+  });
+
 });
